7be1aa9440b602824b9f1319a5f3c6d06fa41e2d
psvishnu91 <psvishnu.91@gmail.com>
2014-12-05 10:31:37 -0500

    adding shell script

---
+++ node_install.sh
@@ -0,0 +1,14 @@
+sudo apt-get install python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+sudo npm install -g express-generator
+express proj
+cd proj
+npm install
+sudo npm install nodemon
+DEBUG=proj ./bin/www
+
+
+installing node oracle
+download sdk and basic copy the files to /opt/instantclient/
